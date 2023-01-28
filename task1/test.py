edbd5463e6861a4bf85f73caa7f971991f4fa2a3
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-01-28 12:12:53 +0300

    Update test.py

--- task1/test.py
+++ task1/test.py
@@ -4,25 +4,31 @@ import numpy as np
 
 from var1.test import test as var1_test
 
-from student_work.task1 import variant
-from student_work.task1 import decision
-
 
 def write_task_res(status, task_score):
     env_file = os.getenv("GITHUB_ENV")
     with open(env_file, "a") as myfile:
         myfile.write(f"status={status}\ntask_score={task_score}\n")
 
+        
+try:
+    from student_work.task1 import variant
+    from student_work.task1 import decision
+except Exception:
+    write_task_res(f"Exception in imports. Type: {type(e)}, messange: {str(e)}", 0)
+    return
+
+
 test = None
 if variant == 1:
     test = var1_test
 else:
     write_task_res(f"Variant {variant} not found", 0)
+    return
 
-if test is not None:
-    try:
-        task_score = test(decision)
-    except Exception as e:
-        write_task_res(f"Exception in decision function. Type: {type(e)}, messange: {str(e)}", 0)
-    else:
-        write_task_res("Done", task_score)
+try:
+    task_score = test(decision)
+except Exception as e:
+    write_task_res(f"Exception in decision function. Type: {type(e)}, messange: {str(e)}", 0)
+else:
+    write_task_res("Done", task_score)
