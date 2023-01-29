616ef64ce61522501c324fa9b0e93dd5bda8fb38
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-01-29 23:12:15 +0300

    Create test.py

---
+++ task1/var2/test.py
@@ -0,0 +1,54 @@
+import os
+import pandas as pd 
+import numpy as np
+
+
+def score(test_stat: dict) -> int:
+    total_score = 0
+
+    if test_stat[1000]["mean_error"] < 0.0003:
+        total_score += 1
+
+    if test_stat[1000]["mean_error"] < 0.0001:
+        total_score += 1
+
+    if test_stat[100]["mean_error"] < 0.0005:
+        total_score += 1
+
+    if test_stat[10]["mean_error"] < 0.0032:
+        total_score += 1
+
+    return total_score
+  
+  
+def test(solution) -> int:
+    data = pd.read_csv("task1/var2/sample.csv")
+    a_sample = data["a"]
+    data_sample = data.drop(columns="a")
+
+    test_stat = {}
+    sample_size_error = {}
+    sample_size_number = {}
+    sample_size_result = {}
+
+    for i in range(len(a_sample)):
+        a = a_sample[i]
+        x = data_sample.iloc[i].dropna().to_numpy()
+        sample_size = len(x)
+
+        a_est = solution(x)
+        error = (a - a_est)**2
+
+        if sample_size in test_stat.keys():
+            test_stat[sample_size]["total_error"] += error
+            test_stat[sample_size]["number"] += 1
+        else:
+            test_stat[sample_size] = {}
+            test_stat[sample_size]["total_error"] = error
+            test_stat[sample_size]["number"] = 1
+
+    for sample_size in test_stat.keys():
+        test_stat[sample_size]["mean_error"] = test_stat[sample_size]["total_error"] \
+                                               / test_stat[sample_size]["number"]
+    
+    return score(test_stat)
