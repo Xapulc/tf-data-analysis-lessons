42632f9f3ab51fd3514a6a341e777139ee43a6c7
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-02-04 02:02:14 +0300

    Create test.py

---
+++ task2/test.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd 
+import numpy as np
+
+from var0.test import test as var0_test
+
+
+def write_task_res(status, task_score):
+    env_file = os.getenv("GITHUB_ENV")
+    max_score = 6
+    answer = f"status={status}\nmax_score={max_score}\ntask_score={task_score}\n"
+    print(answer)
+    with open(env_file, "a") as myfile:
+        myfile.write(answer)
+
+        
+try:
+    from student_work.task2.solution import variant, solution
+except Exception as e:
+    write_task_res(f"Exception in imports. Type: {type(e)}, messange: {str(e)}", 0)
+    quit()
+
+
+test = None
+if variant == 0:
+    test = var0_test
+else:
+    write_task_res(f"Variant {variant} not found", 0)
+    quit()
+
+try:
+    task_score = test(solution)
+except Exception as e:
+    write_task_res(f"Exception in decision function. Type: {type(e)}, messange: {str(e)}", 0)
+else:
+    write_task_res("Done", task_score)
