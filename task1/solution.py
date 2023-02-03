d1383fc67557edc068a7ca4f8623955adf8c054e
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-02-03 23:38:08 +0300

    Create solution.py

---
+++ task1/solution.py
@@ -0,0 +1,10 @@
+
+from var0.solution import solution as var0_solution
+from var1.solution import solution as var1_solution
+from var2.solution import solution as var2_solution
+from var3.solution import solution as var3_solution
+
+variant = 0
+
+def solution(x: np.array):
+    return var0_solution(x)
