ee4825b76b1dfe5b4a66d66dc4ce7001e7d58d63
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-02-14 21:38:23 +0300

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,21 @@
+import email
+import smtplib
+
+
+def send_result(to_email, message_subject, message_body):
+    from_email = os.env....
+    from_email_password = os.env...
+    
+    msg = email.message_from_string(message_body)
+    msg["From"] = from_email
+    msg["To"] = to_email
+    msg["Subject"] = message_subject
+    
+    s = smtplib.SMTP("smtp-mail.outlook.com", 587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(from_email, from_email_password)
+    
+    s.sendmail(from_email, to_email, msg.as_string())
+    s.quit()
