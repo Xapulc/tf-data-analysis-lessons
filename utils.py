65e518e6ac7878d2a40b80e0c26efb482cc7a537
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-02-15 10:09:21 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
+import os
 import email
 import smtplib
 
 
-def send_result(to_email, message_subject, message_body):
+def send_result_to_email(to_email, message_subject, message_body):
     from_email = os.getenv('EMAIL')
     print(from_email)
     from_email_password = os.getenv('PASSWORD')
@@ -20,3 +21,12 @@ def send_result(to_email, message_subject, message_body):
     
     s.sendmail(from_email, to_email, msg.as_string())
     s.quit()
+
+def send_result_to_edu(comment, task_score, max_score):
+    env_file = os.getenv("GITHUB_ENV")
+    answer = f"status={comment}\nmax_score={max_score}\ntask_score={task_score}\n"
+    print(answer)
+    
+    with open(env_file, "a") as myfile:
+        myfile.write(answer)
+    
