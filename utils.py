a1fc7ce6574c9539cce934a76793b1a1e4ea6aa0
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-02-15 16:36:27 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,8 +5,9 @@ import smtplib
 
 def send_result_to_email(to_email, message_subject, message_body):
     from_email = os.getenv("EMAIL")
-    print(from_email)
     from_email_password = os.getenv("PASSWORD")
+    print(len(from_email))
+    print(len(from_email_password))
     
     msg = email.message_from_string(message_body)
     msg["From"] = from_email
