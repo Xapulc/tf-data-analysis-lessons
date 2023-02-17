0b3f5901f1de5f3ccba7feaa000df631d114ea95
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-02-17 11:35:06 +0300

    Update test.py

--- stat_task2/var0/test.py
+++ stat_task2/var0/test.py
@@ -1,36 +1,13 @@
 import os
 import pandas as pd 
 import numpy as np
-
-
-def score(test_stat: dict) -> int:
-    total_score = 0
-
-    if test_stat[0.99][1000]["mean_error"] < 0.02 and test_stat[0.99][1000]["mean_interval_length"] < 2:
-        total_score += 1
-
-    if test_stat[0.9][1000]["mean_error"] < 0.12 and test_stat[0.9][1000]["mean_interval_length"] < 1.1:
-        total_score += 1
-
-    if test_stat[0.7][100]["mean_error"] < 0.32 and test_stat[0.7][100]["mean_interval_length"] < 2.2:
-        total_score += 1
-
-    if test_stat[0.9][100]["mean_error"] < 0.11 and test_stat[0.9][100]["mean_interval_length"] < 3.3:
-        total_score += 1
-
-    if test_stat[0.95][10]["mean_error"] < 0.1 and test_stat[0.95][10]["mean_interval_length"] < 13:
-        total_score += 1
-
-    if test_stat[0.9][10]["mean_error"] < 0.11 and test_stat[0.9][10]["mean_interval_length"] < 10.6:
-        total_score += 1
-
-    return total_score
   
   
-def test(solution) -> int:
+def test(solution) -> list:
     data = pd.read_csv("stat_task2/var0/sample.csv")
     a_sample = data["a"]
     data_sample = data.drop(columns="a")
+    
     score_list = [{
         "sample_size": 1000,
         "confidence": 0.99,
@@ -62,31 +39,26 @@ def test(solution) -> int:
         "max_error": 0.11,
         "max_interval_length": 10.6
     }]
-
-    for p in test_stat.keys():
-        for sample_size in test_stat[p].keys():
-            test_stat[p][sample_size]["total_error"] = 0
-            test_stat[p][sample_size]["total_interval_length"] = 0
-            test_stat[p][sample_size]["number"] = 0
-
-        for i in range(len(a_sample)):
-            a = a_sample[i]
-            x = data_sample.iloc[i].dropna().to_numpy()
-            sample_size = len(x)
-
-            left_side, right_side = solution(p, x)
+    
+    for i in range(len(a_sample)):
+        a = a_sample[i]
+        x = data_sample.iloc[i].dropna().to_numpy()
+        sample_size = len(x)
+        
+        for score_element in score_list:
+            if sample_size != score_element["sample_size"]:
+                continue
+            
+            left_side, right_side = solution(score_element["confidence"], x)
             interval_length = np.abs(right_side - left_side)
             error = 1 if (a < left_side or right_side < a) else 0
+        
+            score_element["total_error"] = score_element.get("total_error", 0) + error
+            score_element["total_interval_length"] = score_element.get("total_interval_length", 0) + interval_length
+            score_element["number"] = score_element.get("number", 0) + 1
+    
+    for score_element in score_list:
+        score_element["mean_error"] = score_element["total_error"] / score_element["number"]
+        score_element["mean_interval_length"] = score_element["total_interval_length"] / score_element["number"]
 
-            if sample_size in test_stat[p].keys():
-                test_stat[p][sample_size]["total_error"] += error
-                test_stat[p][sample_size]["total_interval_length"] += interval_length
-                test_stat[p][sample_size]["number"] += 1
-
-        for sample_size in test_stat[p].keys():
-            test_stat[p][sample_size]["mean_error"] = test_stat[p][sample_size]["total_error"] \
-                                                      / test_stat[p][sample_size]["number"]
-            test_stat[p][sample_size]["mean_interval_length"] = test_stat[p][sample_size]["total_interval_length"] \
-                                                                / test_stat[p][sample_size]["number"]
-
-    return score(test_stat)
+    return score_list
