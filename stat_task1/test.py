b39233a2860dc6c892e701dfd2a2e060c99a88c8
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-02-17 16:47:38 +0300

    Update test.py

--- stat_task1/test.py
+++ stat_task1/test.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
 
 from .var0.test import test as var0_test
 from .var1.test import test as var1_test
@@ -47,3 +49,67 @@ def check_solution(variant, solution):
     message += f"Ваш общий результат: *{task_score} из {max_score}*."
    
     return task_score, message, "Done"
+
+
+
+
+
+    score_data = pd.DataFrame(score_list)
+    score_data["score"] = score_data.apply(lambda row : 1 if row["test_error"] <= row["max_error"] else 0,
+                                           axis=1)
+    score_data["test_error_format"] = score_data.apply(lambda row: "{:." + str(int(-np.log10(row["max_error"])) + 3) + "f}",
+                                                       axis=1)
+    score_data["test_error_str"] = score_data.apply(lambda row: row["test_error_format"].format(row["test_error"]), 
+                                                    axis=1)
+    column_description = [{
+        "column": "sample_size",
+        "description": "Размер выборки"
+    }, {
+        "column": "confidence",
+        "description": "Уровень доверия"
+    }, {
+        "column": "test_error_str",
+        "description": "Среднеквадратичная ошибка решения"
+    }, {
+        "column": "max_error",
+        "description": "Порог для среднеквадратичной ошибки решения"
+    }, {
+        "column": "score",
+        "description": "Балл"
+    }]
+    
+    score_data = score_data[[el["column"] for el in column_description]] \
+                           .rename(columns={el["column"]: el["description"] for el in column_description})
+    
+    color_function = lambda score: "rgba(114, 220, 140, 0.5)" if score == 1 else "rgba(240, 113, 111, 0.5)"
+    cell_height = 30
+    header_cell_height = 70
+    
+    fig = go.Figure(go.Table(header={
+                                 "values": list(score_data.columns),
+                                 "height": header_cell_height
+                             },
+                             cells={
+                                 "values": score_data.values.transpose(),
+                                 "fill_color": [score_data["Балл"].apply(color_function)],
+                                 "height": cell_height
+                             }))
+    fig.update_layout(
+        margin={
+            "l": 0,
+            "r": 0,
+            "t": 0,
+            "b": 0
+        },
+        width=700,
+        height=cell_height * score_data.shape[0] + header_cell_height
+    )
+    picture_path = "./" + salt + ".png"
+    fig.write_image(picture_path)
+
+    task_score = score_data["Балл"].sum()
+    message = f"В задании 1 у вас *{variant}-й вариант*.\n" \
+              + f"Ваш общий результат: *{task_score} из {max_score}*.\n" \
+              + "Итоги проверки результатов подведены в таблице."
+   
+    return task_score, message, "Done", [picture_path]
