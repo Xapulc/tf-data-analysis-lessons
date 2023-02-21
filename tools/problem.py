e0bb636a31db1d95e88131ad27c9a9ad068750e8
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-02-21 22:15:10 +0300

    Update problem.py

--- tools/problem.py
+++ tools/problem.py
@@ -1,56 +1,17 @@
-import hashlib
-
-from abc import ABC, abstractmethod, property
-
-
-class Problem(ABC):
-    def __init__(self):
-        pass
-    
-    @property
-    @abstractmethod
-    def code(self):
-        pass
-    
-    @property
-    @abstractmethod
-    def name(self):
-        pass
-    
-    @property
-    @abstractmethod
-    def max_score(self):
-        pass
-    
-    @property
-    @abstractmethod
-    def problem_variant_list(self):
-        pass
-    
-    @abstractmethod
-    def make_notification(self, user, task_score, test_result):
-        pass
+class Problem(object):
+    def __init__(self, code, name, max_score, problem_variant_list):
+        self.code = code
+        self.name = name
+        self.max_score = max_score
+        self.problem_variant_list = problem_variant_list
         
-    def _get_random_user_number(self, user: str): # убрал в RandomUser
-        hash_string = user + self.name
-        hash_object = hashlib.md5(hash_string.encode("utf8"))
-        hash_int = int.from_bytes(hash_object.digest(), "big")
-        return hash_int
         
-    def _get_problem_variant(self, user: str): # убрал в RandomUser
-        variant_cnt = len(self.problem_variant_list)
-        hash_int = self._get_random_user_number(user)
-        variant = hash_int % variant_cnt
-        return problem_variant_list[variant]
+class ProblemVariant(object):
+    def __init__(self, template):
+        self.template = template
         
-    def get_description(self, user):
-        user = str(user)
-        problem_variant = self._get_problem_variant(user)
-        return problem_variant.get_description(self._get_random_user_number(user))
-    
-    def make_test(self, solution, user):
-        user = str(user)
-        problem_variant = self._get_problem_variant(user)
-        task_score, comment, test_result = problem_variant.make_test(solution, self._get_random_user_number(user))
-        self.make_notification(user, task_score, test_result)
-        return task_score, comment
+        
+class GeneratedProblemVariant(object):
+    def __init__(self, random_state, description):
+        self.random_state = random_state
+        self.description = description
