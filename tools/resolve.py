4938fdf883b742ecc68e2571c28a49997c8d9532
vi.kharlamov <vi.kharlamov@tinkoff.ru>
2023-02-26 21:54:00 +0300

    Убрал нулевой вариант для всех, кроме автора задачи.

--- tools/resolve.py
+++ tools/resolve.py
@@ -18,6 +18,6 @@ class UserVariantResolver(object, metaclass=Singleton):
         else:
             hash_int = self.get_number(user, problem)
             element_cnt = len(problem.problem_variant_list)
-            element_num = hash_int % element_cnt
+            element_num = 1 + (hash_int % (element_cnt - 1))
 
         return problem.problem_variant_list[element_num]
